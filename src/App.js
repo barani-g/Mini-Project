bad1271d6726cebe7e1615a50124738d18a9db76
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-02-06 13:02:59 +0530

    Containers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import Admin from './Admin';
 import './App.css';
-import Manager from './Manager';
-import User from './User';
+import Manager from './Containers/Manager';
+import User from './Containers/User';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './Containers/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/' element={<Home></Home>}></Route>
           <Route path='/User' element={<User></User>}></Route>
           <Route path='/Admin' element={<Admin></Admin>}></Route>
           <Route path='/Manager' element={<Manager></Manager>}></Route>
