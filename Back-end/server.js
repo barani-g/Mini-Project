5159705358eedcd120e2cce81eb463a0c0b2359e
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-02-24 16:48:18 +0530

    Remove Node Modules

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,6 +1,7 @@
 // Import Packages
 const express = require('express'); //Framework for APIs
 const cors = require('cors'); //Provides Express Middleware
+const mongoose = require('mongoose');
 
 //Dotenv - For Connection
 
@@ -13,6 +14,15 @@ const port = process.env.PORT || 8000;
 //Middlewares
 app.use(cors());
 app.use(express.json());
+
+//Connecting to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("MongoDB has been connected successfully!");
+})
+
 
 
 app.listen(port,() => {
