9d8bdcf6421f1fafbbb73d7639d87b927502f51a
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-03-16 13:31:31 +0530

    Adding Image to Restaurants

--- Front-end/src/Screens/HomeScreens/UserHome.js
+++ Front-end/src/Screens/HomeScreens/UserHome.js
@@ -8,19 +8,34 @@ function UserHome() {
     const UserDetails = location.state;
   return (
     <div >
-        <div class="flex flex-col items-start m-40 ">
+        <div class="flex flex-col items-start mt-40 ml-40 ">
             <span class="text-2xl"> Welcome 
                 <span class="font-bold"> {UserDetails.username}</span>
             </span>
         </div>
         
-        <div class="flex flex-col min-h-screen items-center mt-40">
-            <ul>
+        <div class="flex flex-col items-center mt-6">
                 <h1 class="text-3xl font-bold mb-10">Pick Restaurant </h1>
-                    <Link to="/UserRestaurantHome">
-                    <li class="p-10 rounded-lg text-white bg-mypurple shadow-lg font-bold text-2xl">Arasan</li>
-                    </Link>
-                </ul>
+
+                {/* Button Layout */}
+                <div class="flex gap-10">
+                {/* Restaurant 1 Button */}
+                <Link to="/UserRestaurantHome">
+                <div class="relative shadow-lg font-bold text-2xl">
+                    <img src={require("/Users/gnaneshwaran/Documents/GitProjects/Mini-Project/my-app/Front-end/src/Images/Arasan.jpg")} class="rounded-lg w-[400px] h-[300px]"></img>
+                    <h1 class="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Arasan</h1>
+                </div>
+                </Link>
+
+                {/* Restaurant 2 Button */}
+                <div class="relative shadow-lg font-bold text-2xl">
+                    <img src={require("/Users/gnaneshwaran/Documents/GitProjects/Mini-Project/my-app/Front-end/src/Images/Darling.jpg")} class="rounded-lg w-[400px] h-[300px]"></img>
+                    <h1 class="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Darling</h1>
+                </div>
+                
+                </div>
+                
+                
         </div>
             
     </div>
