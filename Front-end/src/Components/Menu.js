d779804f530fe3ddc9ff4b206aeebca6faee7ac8
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-03-16 15:33:09 +0530

    minor

--- Front-end/src/Components/Menu.js
+++ Front-end/src/Components/Menu.js
@@ -5,7 +5,7 @@ import ProductItem from './ProductItem'
 function Menu() {
   return (
     <div class="bg-gray-100 ">
-      <div class=" flex justify-center text-5xl font-bold pt-5 ">
+      <div class=" flex justify-center  text-5xl font-bold pt-5 pb-5 overline">
         Menu
       </div>
       
