17e979923cbb99efbb6504f983110c53947760bb
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-03-16 09:59:26 +0530

    Adding Comments

--- Front-end/src/Screens/HomeScreens/UserHome.js
+++ Front-end/src/Screens/HomeScreens/UserHome.js
@@ -20,27 +20,25 @@ function UserHome() {
   const handleLogout = (e) => {
     navigate('/');
   }  
+  
   return (
     <div>
       UserHome
       <div>{UserDetails.username}</div>
 
       {/* Side Bar */}
-      <span
-      class="absolute text-white text-4xl top-5 left-4 cursor-pointer"
-      onclick="openSidebar()"
-    >
-      <i class="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
-    </span>
     <div
       class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-white shadow-lg"
     >
+      {/* Restaurant Logo */}
       <div class="text-black text-xl">
         <div class="p-2.5 mt-1 flex items-center">
           <h1 class="font-bold text-black text-3xl ml-3">Arasan</h1>
         </div>
         <div class="my-2 bg-gray-600 h-[1px]"></div>
       </div>
+
+      {/* Search Bar */}
       <div
         class="mt-5 p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-black text-white"
       >
@@ -51,21 +49,27 @@ function UserHome() {
           class="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
         />
       </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-mypurple text-black hover:text-white "
+
+      {/* Menu Button */}
+      <button
+        class="p-2.5 mt-3 flex w-full items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-mypurple text-black hover:text-white "
       >
         <img class="aspect-square " src={MenuImg} />
         <span class="text-[15px] ml-4 font-bold">Menu</span>
-      </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-mypurple text-black hover:text-white"
+      </button>
+
+      {/* Order Button */}
+      <button
+        class="p-2.5 mt-3 flex w-full items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-mypurple text-black hover:text-white"
       >
         <img class="aspect-square " src={OrderImg} />
         <span class="text-[15px] ml-4 font-bold">Order</span>
-      </div>
+      </button>
+
+      {/* Divider */}
       <div class="my-4 bg-gray-600 h-[1px]"></div>
       
-      
+      {/* Logout Button */}
       <button
         class="p-2.5 mt-3 flex w-full items-center rounded-md px-4 duration-300 hover:bg-mypurple text-black hover:text-white"
         onClick={handleLogout}
